f25da513d91d31be9ff50eff6d5a70ce732fb230
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-11 21:13:27 +0530

    Adding outputs

--- output.tf
+++ output.tf
@@ -1,1 +1,13 @@
+output "az_public_ip_address"{
+  description = "Public address of the created Public IP"
+  value       =  azurerm_public_ip.az_public_ip_address.ip_address
+}
+output "az_public_ip_id"{
+  description = "Public IP address id"
+  value       = azurerm_public_ip.az_public_ip_id.id
+}
+output "az_public_ip_name"{
+  description = "Name of newly created Public IP"
+  value       = var.name
+}
 
