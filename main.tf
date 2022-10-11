29ac54569c7b65e5b2fb3fb482ff851bf078bd38
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-11 21:04:23 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-
+data "azurem_resource_group" "rg" { 
+  name = var.resource_group_name
+}
+resource "azurerm_public_ip" "az_public_ip" {
+   name                = var.name
+   resource_group_name = data.azurerm_resource_group_name.rg.name
+   location            = data.azruerm_resource_group_name.rg.location
+   allocation_method   = var.allocation
+  ip_version           = var.ip_version
+}
+   
+   
