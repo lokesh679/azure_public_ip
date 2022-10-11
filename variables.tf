09cb8f545d39181b0ce75cd8ec909c5149063b71
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-11 20:58:36 +0530

    Declared variables to create Azure Public IP

--- variables.tf
+++ variables.tf
@@ -1,1 +1,17 @@
+variable "azure_resource_group" {
+   description = "(Required) The name of the resource group"
+   type        = string 
+}
+variable "name" {
+  description  = "(Required) The name of the Public IP"
+  default      = "cloud-Public-IP"
+}
+variable "allocation" { 
+  description  = "Possible Values are Static or Dynamic"
+  default      = "cloud-security-rule"
+}
+variable "ip_version"{
+  description  = "Version of the Public IP"
+  default      = "IPv4"
+}
 
